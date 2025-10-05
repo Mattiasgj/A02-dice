4a8beb7cbfda0cc7320a8da569832b81153c081e
Mattias <gillm7623@gmail.com>
2025-10-05 11:07:31 +0200

    Added the class files and a mian.py file

---
+++ game.py
@@ -0,0 +1,5 @@
+class Game:
+    def __init__(self, player_name = "player", player2_name = "computer", target_score = 100):
+        self.player_name = player_name
+        self.player2_name = player2_name
+        self.target_score = target_score
