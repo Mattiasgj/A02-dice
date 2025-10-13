65f20f76af9d2e374d8eb6e0bbced3e4ca3907ac
Mattiasgj <gillm7623@gmail.com>
2025-10-13 10:01:55 +0200

    added basic methods to game.py, player.py and made a simple example with main.py

--- dice_game/dice_hand.py
+++ dice_game/dice_hand.py
@@ -1,13 +1,12 @@
-from dice import Dice
+from dice_game.dice import Dice
 
 class DiceHand:
     def __init__(self, numb_dice: int = 1):
         self.dice = [Dice() for _ in range(numb_dice)]
         self.lastRoll = []
-   def roll(self):
+    def roll(self):
         self.lastRoll = [die.roll() for die in self.dice]
         return self.lastRoll
 
     def __str__(self):
         return " ".join(str(die) for die in self.lastRoll)
-    
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from dice_game.game import Game
-
+from dice_game.player import Player
 def main():
-    game = Game()
+    player_1 = Player("test 1")
+    player_2 = Player("test 2")
 
-    print(game.is_game_over(90, 90))
+    game = Game()
+    
+    hold = False
+
+    while True:
+        turn = game.play_turn(player_1, hold)
+        print(turn)
+        if turn == False:
+            print("turn over")
+            break
+        else:
+            print(f"Your score is: {player_1.get_score()}")
+
+        hold = input('Hold? y/n: ').lower().startswith('y')
+        
+        
+
+
    
 
 if __name__ == "__main__":
