f260b01f07b1e8769db851d7f68306bd4ad4d237
Mattiasgj <gillm7623@gmail.com>
2025-10-13 10:32:14 +0200

    added switch_turns method to main for switching between turns

--- dice_game/game.py
+++ dice_game/game.py
@@ -4,8 +4,16 @@ class Game:
     target_score = 20
     dice = DiceHand()
 
-    def __init__(self):
-        pass
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def switch_turns(self, current_player):
+        if current_player == self.player_1:
+            return self.player_2
+        elif current_player == self.player_2:
+            return self.player_1
+
 
     def play_turn(self, player, hold):
         if hold == False:
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from dice_game.game import Game
 from dice_game.player import Player
+
 def main():
     player_1 = Player("test 1")
     player_2 = Player("test 2")
 
-    game = Game()
+    game = Game(player_1, player_2)
     
     hold = False
 
+    current_player = player_1
+    
     while True:
-        turn = game.play_turn(player_1, hold)
+        turn = game.play_turn(current_player, hold)
         print(turn)
         if turn == False:
-            print("turn over")
-            break
+            current_player = game.switch_turns(current_player)
+            print(f"turn over, now its {current_player.get_name()} turn")
         else:
-            print(f"Your score is: {player_1.get_score()}")
+            print(f"Your score is: {current_player.get_score()}")
 
         hold = input('Hold? y/n: ').lower().startswith('y')
         
